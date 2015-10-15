9eb874121f80d5183f9bf386aaccfc80801e593e
noahwaterfieldprice <noahwaterfieldprice@gmail.com>
2015-10-15 10:58:27 +0100

    Initial commit - basic package structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# diffraction
+
+**diffraction** is an open-source Python library for simulating x-ray and neutron diffraction experiments and performing crystallographic calculations.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='diffraction',
+    version='0.1',
+    url='http://github.com/noahwaterfieldprice/diffraction/',
+    author='Noah Waterfield Price',
+    author_email='noah.waterfieldprice@physics.ox.ac.uk',
+    description='A package for simulating diffraction experiments'
+                'and performing crystallographic calculations.',
+    packages=['diffraction']
+)
